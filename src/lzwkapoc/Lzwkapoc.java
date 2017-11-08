2658a4bceb78ecd9a80303b920bb4b811e816884
cristian-gil <cristian-gil@B-SG701-08.upiloto.edu>
2017-11-07 19:30:34 -0500

    Formulario inicial lzw

---
+++ src/lzwkapoc/Lzwkapoc.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lzwkapoc;
+
+import presentation.lzwform;
+
+/**
+ *
+ * @author cristian-gil
+ */
+public class Lzwkapoc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        lzwform kapoc = new lzwform();
+        kapoc.show();
+    }
+    
+}
